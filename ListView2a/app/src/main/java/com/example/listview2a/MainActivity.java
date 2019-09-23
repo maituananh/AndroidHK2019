8f5d2be6e368587af9b9183402dd4270ebcb66e2
01224349901Anh <maituananh>
2019-09-23 11:37:43 +0700

    done List view 2a

--- ListView2a/app/src/main/java/com/example/listview2a/MainActivity.java
+++ ListView2a/app/src/main/java/com/example/listview2a/MainActivity.java
@@ -10,13 +10,15 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ListView listView;
-    ArrayList<String> stringArrayList;
+    private ArrayList<String> stringArrayList = new ArrayList<>();
+    private int i = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,23 +41,50 @@ public class MainActivity extends AppCompatActivity {
 
     public void addListView() {
         listView = findViewById(R.id.listViewQuocGia);
-        stringArrayList = new ArrayList<>();
-        stringArrayList.add("VIET NAM");
-        stringArrayList.add("THAI LAN");
-        stringArrayList.add("TRUNG QUOC");
-        stringArrayList.add("LAO");
-        stringArrayList.add("TRIEU TIEN");
-        stringArrayList.add("USA");
-        stringArrayList.add("ANH");
-        stringArrayList.add("NHAT BAN");
-        stringArrayList.add("HAN QUOC");
-        stringArrayList.add("THUY DIEN");
-        stringArrayList.add("MONG CO");
-        stringArrayList.add("SINGAPO");
+        stringArrayList.add(tangID() + " - VIET NAM");
+        stringArrayList.add(tangID() + " - THAI LAN");
+        stringArrayList.add(tangID() + " - TRUNG QUOC");
+        stringArrayList.add(tangID() + " - LAO");
+        stringArrayList.add(tangID() + " - TRIEU TIEN");
+        stringArrayList.add(tangID() + " - USA");
+        stringArrayList.add(tangID() + " - ANH");
+        stringArrayList.add(tangID() + " - NHAT BAN");
+        stringArrayList.add(tangID() + " - HAN QUOC");
+        stringArrayList.add(tangID() + " - THUY DIEN");
+        stringArrayList.add(tangID() + " - MONG CO");
+        stringArrayList.add(tangID() + " - SINGAPO");
+        stringArrayList.add(tangID() + " - AO");
+        addAdapter();
+    }
 
+    public void actionADD(View view) {
+        EditText editTextADD = findViewById(R.id.textADD);
+        String add = String.valueOf(editTextADD.getText());
+        stringArrayList.add(tangID() +" - "+ add);
+        addAdapter();
+        editTextADD.setText("");
+    }
+
+    public void actionEdit(View view) {
+        EditText editTextID = findViewById(R.id.textIDEDIT);
+        EditText editTextEdit = findViewById(R.id.textEDIT);
+        String id = String.valueOf(editTextID.getText());
+        String textEdit = String.valueOf(editTextEdit.getText());
+        stringArrayList.set(Integer.parseInt(id.trim()), id + " - " + textEdit);
+        addAdapter();
+        editTextID.setText("");
+        editTextEdit.setText("");
+    }
+
+    public void addAdapter() {
         ArrayAdapter arrayAdapter = new ArrayAdapter(MainActivity.this,
                 android.R.layout.simple_list_item_1,
                 stringArrayList);
         listView.setAdapter(arrayAdapter);
+    }
+
+    public int tangID() {
+        this.i += 1;
+        return this.i;
     }
 }
